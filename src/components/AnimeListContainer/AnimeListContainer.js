124c6818e22b7c39e2396c110338a4ee119795e1
Samrat Mukherjee <samratmukherjee.sps@gmail.com>
2021-08-09 14:16:46 +0530

    restructed css code

--- src/components/AnimeListContainer/AnimeListContainer.js
+++ src/components/AnimeListContainer/AnimeListContainer.js
@@ -14,52 +14,17 @@ const AnimeListContainer = ({
   return (
     <div
       className="anime-list-container-bg-color"
-      style={{
-        borderRadius: "12px",
-        backgroundImage: `url(${bannerImg})`,
-        backgroundSize: "cover",
-        width: "660px",
-        height: "200px",
-        zIndex: 2,
-        display: "flex",
-      }}
+      style={{ backgroundImage: `url(${bannerImg})` }}
     >
       <div
         className="alc-cover-img"
         style={{
-          borderRadius: "10px 0 0 10px",
-          width: "150px",
-          height: "200px",
           backgroundImage: `url(${coverImg})`,
-          backgroundSize: "cover",
-          opacity: 1,
-          zIndex: 1,
         }}
       ></div>
-      <div
-        className="anime-list-container"
-        style={{
-          borderRadius: "0 10px 10px 0",
-          filter: "grayscale(90%)",
-          backgroundColor: "white",
-          opacity: "0.9",
-        }}
-      >
-        <div
-          className="alc-data"
-          style={{
-            height: "200px",
-            width: "485px",
-            padding: "20px 0",
-            margin: "0 20px",
-          }}
-        >
-          <h2
-            className="alc-title"
-            style={{ color: "black", padding: "2px 0" }}
-          >
-            {title}
-          </h2>
+      <div className="anime-list-container">
+        <div className="alc-data">
+          <h2 className="alc-title">{title}</h2>
           <div className="alc-description">
             {description["en"] !== null && description["en"] !== "" ? (
               <p>{description["en"]}</p>
